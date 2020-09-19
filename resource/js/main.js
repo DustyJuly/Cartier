725dc3b10b9e4c5b442b99af33ae73ca4f12903a
DustyJuly <juliet.90@mail.ru>
2020-09-19 23:47:55 +0300

    Add files via upload

---
+++ resource/js/main.js
@@ -0,0 +1,21 @@
+$(function(){
+  new WOW().init();
+
+  if ($(window).width() < 1380) {
+    $('body').addClass('hidden');
+} else {
+    $('body').removeClass('hidden');
+}
+ 
+  const mMenuBtn = $(".burger__menu");
+  const mMenu = $(".mobile__menu");
+
+
+  mMenuBtn.on('click', function() {
+    mMenu.toggleClass('active');
+    $('body').toggleClass("no-scroll")
+  });
+
+
+
+});
